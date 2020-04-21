73f8276e02275f0cd2be566d510e9060193ff6f0
Alexandru Cotin <a.cotin96@gmail.com>
2020-04-21 14:30:42 +0200

    first commit

---
+++ counter app/src/App.js
@@ -0,0 +1,61 @@
+import React, { Component, useState, useEffect } from "react";
+
+
+//=== WITH HOOKS ===//
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() =>{
+    document.title = `Clicked ${count} times`;
+  })
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h2>Counter app</h2>
+      <button onClick={increment}>Clicked {count} times</button>
+    </div>
+  );
+};
+
+
+
+
+//=== WITHOUT HOOKS ===//
+
+// class App extends Component {
+//   state = {
+//     count: 0,
+//   };
+
+//   increment = () => {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   };
+
+//   componentDidMount() {
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+
+//   componentDidUpdate() {
+//     document.title = `Clicked ${this.state.count} times`;
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h2>Counter app</h2>
+//         <button onClick={this.increment}>
+//           Clicked {this.state.count} times
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
