c3ebd64d0d577719a25fc7d095bb556a0b08e386
Alexandru Cotin <a.cotin96@gmail.com>
2020-04-21 15:10:46 +0200

    last commit for react hooks

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -1,27 +1,54 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const App = () => {
   //state
   const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("react");
+  const [url, setUrl] = useState(
+    "http://hn.algolia.com/api/v1/search?query=react"
+  );
+  const [loading, setLoading] = useState(false);
 
   //fetch news
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    //set loading true
+    setLoading(true);
+    fetch(url)
       .then((result) => result.json())
-      .then((data) => setNews(data.hits))
+      .then((data) => (setNews(data.hits), setLoading(false)))
       .catch((error) => console.log(error));
   };
 
   useEffect(() => {
     fetchNews();
-  });
+  }, [url]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  };
+
+  const showLoading = () => (loading ? <h2>Loading...</h2> : "");
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={searchQuery} onChange={handleChange} />
+      <button>Search</button>
+    </form>
+  );
+
+  const searchNews = () => (news.map((n, i) => <p key={i}>{n.title}</p>));
 
   return (
     <div>
       <h2>News</h2>
-      {news.map((n, i) => (
-        <p key={i}>{n.title}</p>
-      ))}
+      {showLoading()}
+      {searchForm()}
+      {searchNews()}
     </div>
   );
 };
